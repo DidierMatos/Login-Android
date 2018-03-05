dda10eab9523b961f6fc24b972edc8656c78aaf7
DidierMatos <Didiermat00smail.com>
2018-03-05 10:39:39 -0500

    Conexión y validación de datos

---
+++ app/src/main/java/com/simulogin/innovasoftdl/login_android/RegisterRequest.java
@@ -0,0 +1,31 @@
+package com.simulogin.innovasoftdl.login_android;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by didie on 05/03/2018.
+ */
+
+public class RegisterRequest extends StringRequest {
+
+    private static final String REGISTER_REQUEST_URL="http://localhost/Register.php";
+    private Map<String,String> params;
+    public RegisterRequest(String name, String username, int age, String pasword, Response.Listener<String> listener){
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params=new HashMap<>();
+        params.put("name", name);
+        params.put("username", username);
+        params.put("age", age + "");
+        params.put("password", pasword);
+
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
--- app/src/main/java/com/simulogin/innovasoftdl/login_android/Registro.java
+++ app/src/main/java/com/simulogin/innovasoftdl/login_android/Registro.java
@@ -1,13 +1,22 @@
 package com.simulogin.innovasoftdl.login_android;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.simulogin.innovasoftdl.login_android.R;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Registro extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener {
 
     EditText etnombre, etusuario, etpassword, etedad;
     Button btn_registrar;
@@ -24,12 +33,41 @@ public class Registro extends AppCompatActivity {
 
         btn_registrar = findViewById(R.id.btn_registrar);
 
-        btn_registrar.setOnClickListener(new View.OnClickListener() {
+        btn_registrar.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        final String name = etnombre.getText().toString();
+        final String username = etusuario.getText().toString();
+        final String password = etpassword.getText().toString();
+        final int age = Integer.parseInt(etedad.getText().toString());
+
+        Response.Listener<String> respoListener = new Response.Listener<String>() {
             @Override
-            public void onClick(View view) {
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean(("success"));
 
+                    if (success){
+                        Intent intent = new Intent(Registro.this,MainActivity.class);
+                        Registro.this.startActivity(intent);
+                    }else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+                        builder.setMessage("Error de registro").setNegativeButton("Retry", null).create().show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        });
+        };
 
+        RegisterRequest registerRequest = new RegisterRequest(name,username, age, password,respoListener);
+        RequestQueue queue = Volley.newRequestQueue(Registro.this);
+        queue.add(registerRequest);
     }
 }
